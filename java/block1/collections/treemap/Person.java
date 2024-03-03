fafd037777b28cdb19e8ec26ab763617175cd2f3
VasiliyVelikyy <algoritmick.vm@gmail.com>
2024-03-03 22:55:22 +0300

    add multithreading examples

---
+++ java/block1/collections/treemap/Person.java
@@ -0,0 +1,13 @@
+package block1.collections.treemap;
+
+public class Person {
+    int age;
+    String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+
+}
