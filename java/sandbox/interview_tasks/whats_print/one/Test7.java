cc711d8461a13462833e33c27d523476469a4947
Vasiliy <algoritmick.vm@gmail.com>
2025-01-29 23:26:59 +0300

    add test questions

--- java/sandbox/interview_tasks/whats_print/one/Test7.java
+++ java/sandbox/interview_tasks/whats_print/one/Test7.java
@@ -1,7 +1,19 @@
 package sandbox.interview_tasks.whats_print.one;
 
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 public class Test7 {
     public static void main(String[] args) {
+        String[] names = {"Java", "Kotlin", "Java"};
+        String name = "Java";
+        Predicate predicate = name::equals;
 
+        long count1 = Stream.of(names).filter(predicate).count();
+        System.out.println("count1=" + count1);
+        name = "Kotlin";
+
+        long count2 = Stream.of(names).filter(predicate).count();//у predicate не меняется ссылка, в отличие от name
+        System.out.println("count2=" + count2);
     }
 }
