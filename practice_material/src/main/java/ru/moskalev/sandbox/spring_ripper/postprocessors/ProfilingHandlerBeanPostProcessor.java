13f91a3d5755f1fc960a34224c95b2ed5f5c6e48
Vasiliy <algoritmick.vm@gmail.com>
2025-02-13 01:28:23 +0300

    refactoring

--- practice_material/src/main/java/ru/moskalev/sandbox/spring_ripper/postprocessors/ProfilingHandlerBeanPostProcessor.java
+++ practice_material/src/main/java/ru/moskalev/sandbox/spring_ripper/postprocessors/ProfilingHandlerBeanPostProcessor.java
@@ -6,16 +6,14 @@ import org.springframework.stereotype.Component;
 import ru.moskalev.sandbox.spring_ripper.annotations.Profiling;
 import ru.moskalev.sandbox.spring_ripper.service.ProfilingController;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
-    private Map<String, Class> map = new HashMap<>();
-    private ProfilingController controller = new ProfilingController();
+    private final Map<String, Class> map = new HashMap<>();
+    private final ProfilingController controller = new ProfilingController();
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
@@ -31,22 +29,18 @@ public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
         var clazz = map.get(beanName);
         if (clazz != null) {
             //любой класс знает какой загрузчик классов его грузил
-            return Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(), new InvocationHandler() {
-                @Override
-                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    if (controller.isEnabled()) {
-                        System.out.println("I am profiling.....");
-                        long before = System.nanoTime();
-                        Object retVal = method.invoke(bean, args);
-                        long after = System.nanoTime();
-                        System.out.println( after - before);
-                        System.out.println("End profiling");
+            return Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(), (proxy, method, args) -> {
+                if (controller.isEnabled()) {
+                    System.out.println("I am profiling.....");
+                    long before = System.nanoTime();
+                    Object retVal = method.invoke(bean, args);
+                    long after = System.nanoTime();
+                    System.out.println(after - before);
+                    System.out.println("End profiling");
 
-                        return retVal;
-                    }
-                    else {
-                       return method.invoke(bean, args);
-                    }
+                    return retVal;
+                } else {
+                    return method.invoke(bean, args);
                 }
             });
         }
