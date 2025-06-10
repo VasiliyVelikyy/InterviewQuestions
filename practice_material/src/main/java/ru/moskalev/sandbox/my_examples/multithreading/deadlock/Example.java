66d19ca8d2288ed215cfe497fec0d2faf7c4c16f
Vasiliy <algoritmick.vm@gmail.com>
2025-06-10 16:54:52 +0300

    save changes

--- practice_material/src/main/java/ru/moskalev/sandbox/my_examples/multithreading/deadlock/Example.java
+++ practice_material/src/main/java/ru/moskalev/sandbox/my_examples/multithreading/deadlock/Example.java
@@ -4,11 +4,11 @@ package ru.moskalev.sandbox.my_examples.multithreading.deadlock;
      public static void main(String[] args) {
 
      }
-     public static int sumSize(List<String> list1, List<String> list2) {
-         synchronized (list1) {
-             synchronized (list2) {
-                 return list1.size() + list2.size();
-             }
-         }
-     }
+//     public static int sumSize(List<String> list1, List<String> list2) {
+//         synchronized (list1) {
+//             synchronized (list2) {
+//                 return list1.size() + list2.size();
+//             }
+//         }
+//     }
 }
